48fbba7266c206b25ebdf24f7ea103257a0056f6
Alexandra Dumitru <alexandra.dumitru2811@gmail.com>
2022-03-23 11:34:07 +0200

    class Ship done

--- DataModel.cs
+++ DataModel.cs
@@ -42,27 +42,141 @@ namespace Codecool.Battleship.DataModel {
 		
 		{
 			List <Location> locationList = new List<Location>();
-			
-			
+			switch (direction)
+			{
+				case (int)ShipDirection.Up:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x, location.y - i);
+							Location placeShip1 = new Location(location.x+1, location.y - i);
+							Location placeShip2 = new Location(location.x-1, location.y - i);
+							locationList.Add(placeShip);
+							locationList.Add(placeShip1);
+							locationList.Add(placeShip2);
+.					    }
+						Location placeShipDown = new Location(location.x, location.y+1);
+						Location placeShipUp = new Location(location.x, location.y - size);
+						locationList.Add(placeShipDown);
+						locationList.Add(placeShipUp);
+						break;
+					}
+				case (int)ShipDirection.Down:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x, location.y + i);
+							Location placeShip1 = new Location(location.x + 1, location.y + i);
+							Location placeShip2 = new Location(location.x - 1, location.y + i);
+							locationList.Add(placeShip);
+							locationList.Add(placeShip1);
+							locationList.Add(placeShip2);
+.					    }
+						Location placeShipDown = new Location(location.x, location.y - 1);
+						Location placeShipUp = new Location(location.x, location.y + size);
+						locationList.Add(placeShipDown);
+						locationList.Add(placeShipUp);
+						break;
+					}
+				case (int)ShipDirection.Left:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x - i, location.y);
+							Location placeShip1 = new Location(location.x - i, location.y + 1);
+							Location placeShip2 = new Location(location.x - i, location.y - 1);
+							locationList.Add(placeShip);
+							locationList.Add(placeShip1);
+							locationList.Add(placeShip2);
+.					    }
+						Location placeShipLeft = new Location(location.x + 1, location.y);
+						Location placeShipRight = new Location(location.x - size, location.y);
+						locationList.Add(placeShipLeft);
+						locationList.Add(placeShipRight);
+						break;
+					}
+				case (int)ShipDirection.Right:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x + i, location.y);
+							Location placeShip1 = new Location(location.x + i, location.y - 1);
+							Location placeShip2 = new Location(location.x + i, location.y + 1);
+							locationList.Add(placeShip);
+							locationList.Add(placeShip1);
+							locationList.Add(placeShip2);
+.					    }
+						Location placeShipLeft = new Location(location.x - 1, location.y);
+						Location placeShipRight = new Location(location.x + size, location.y);
+						locationList.Add(placeShipLeft);
+						locationList.Add(placeShipRight);
+						break;
+					}
+
+			}
+
 			return locationList;
 		}
 		public List<Location> GetFieldMap()
 		{ 
-			
+			List<Location> locations = new List<Location>();
+
+			switch(direction)
+			{
+				case (int)ShipDirection.Up:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x, location.y - i);
+							locations.Add(placeShip);
+.					    }
+						break;
+					}
+				case (int)ShipDirection.Down:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x, location.y + i);
+							locations.Add(placeShip);
+						}
+						break;
+					}
+				case (int)ShipDirection.Left:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x - i, location.y);
+							locations.Add(placeShip);
+						}
+						break;
+					}
+				case (int)ShipDirection.Right:
+					{
+						for (int i = 0; i < size; i++)
+						{
+							Location placeShip = new Location(location.x + i, location.y);
+							locations.Add(placeShip);
+						}
+						break;
+					}
+					
+			}
+			return locations;
+
+
 		}
 
-		// coordonatele shipului
+		
 	}
 
-	public enum ShipDirection
-	{ 
-		None = 0,
-		Up = 1,
-		Right = 2,
-		Down = 3,
-		Left = 4,
-	}
-	public class Player {
+    public enum ShipDirection
+    {
+        Up = 1,
+        Right = 2,
+        Down = 3,
+        Left = 4,
+    }
+    public class Player {
 		public List<Ship> ships;
 		public List<Location> hits, misses, sunks;
 		public bool IsAI;
